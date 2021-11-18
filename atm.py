189c9c6abc43ba3285492d6225fc8c486f53252d
SirDankenstein <52797753+SirDankenstien@users.noreply.github.com>
2021-11-18 21:30:49 +0530

    Update atm.py

--- atm.py
+++ atm.py
@@ -1,3 +1,9 @@
+import hmac
+import json
+import time
+import base64
+import hashlib
+import requests
 from flask import Flask, render_template, request, flash
 
 app = Flask(__name__)
@@ -6,21 +12,9 @@ app = Flask(__name__)
 def index():
     return render_template("index.html")
 
-import hmac
-import hashlib
-import base64
-import json
-import time
-import requests
-
-# Enter your API Key and Secret here. If you don't have one, you can generate it from the website.
 key = "6d3089ee4d8ccf4d062d85069b6378311cce7b25c8ddb8be"
 secret = "02242fac9072ad4077bb1fa623fbf6699b6f43476fbd82a45c5680d6ea4f74eb"
-
-# python3
 secret_bytes = bytes(secret, encoding='utf-8')
-
-# Generating a timestamp
 timeStamp = int(round(time.time() * 1000))
 
 body = {
@@ -28,10 +22,7 @@ body = {
 }
 
 json_body = json.dumps(body, separators = (',', ':'))
-
 signature = hmac.new(secret_bytes, json_body.encode(), hashlib.sha256).hexdigest()
-
-url = "https://api.coindcx.com/exchange/v1/users/balances"
 
 headers = {
     'Content-Type': 'application/json',
@@ -39,7 +30,7 @@ headers = {
     'X-AUTH-SIGNATURE': signature
 }
 
-response = requests.post(url, data = json_body, headers = headers)
+response = requests.post("https://api.coindcx.com/exchange/v1/users/balances", data = json_body, headers = headers)
 data = response.json();
 print(data);
 
