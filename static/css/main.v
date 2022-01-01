f831865837f044b0557585fa478283dcc0f259b3
Veltabex <62775096+Veltabex@users.noreply.github.com>
2022-01-02 00:10:48 +0530

    Add files via upload

---
+++ static/css/main.v
@@ -0,0 +1,17 @@
+body {
+	background-color: slategray;
+	text-align: center;
+}
+
+p {
+	color: #9b1d1d;
+	font: Serif;
+	font-size: 1.2em;
+
+}
+
+img {
+	margin: 60px 0 30px 0;
+	width: 500px;	
+	text-align: center;
+}
