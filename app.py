fa478205b70bf7dfe6faeaa7a48f34e5892533f5
Veltabex <62775096+Veltabex@users.noreply.github.com>
2022-01-27 15:55:51 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -2,5 +2,6 @@ from flask import Flask
 from app_blueprint import app_blueprint
 
 app = Flask(__name__)
+app.secret_key = 'somesecretkeythatonlyishouldknow'
 
 app.register_blueprint(app_blueprint)
