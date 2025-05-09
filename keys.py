f494d9393450e0141043ab069252be795ee53586
Veltabex <62775096+Veltabex@users.noreply.github.com>
2025-05-09 15:26:31 +0100

    Delete keys.py

--- keys.py
+++
@@ -1,9 +0,0 @@
-########################### Binacne API KEYS ###################################
-api_key = "g257kS41SLmFPNWi1tUaFbPSTjiKYj3Rb1D4kLuHFsgAmsn5FoZ628E6yXvtYaV7"
-api_secret = "935PKy1hPks2MTA5SuItUeB9QT4A2ZFItGySJkyc2wdIkVdos2S0jYNawbH0IjIU"
-############################ PAYTM Keys ###################################
-merchant_id = "MVFOAP42279449625656"
-merchant_key = "fh8BvsjqHnZfxVo9"
-############################ Binance Username and password ###################################
-username = 'akbarmirza2001@gmail.com'
-password = 'ErenYeahgaaaa1'
